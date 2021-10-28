8baf9b3e176b919f2c193317843f045fd1481451
Ana Ea <aabad0464@gmail.com>
2021-10-28 13:57:29 -0600

    setting scenarios correct input & no input

--- script.js
+++ script.js
@@ -13,5 +13,16 @@ let secretNumber =Math.trunc(Math.random()* 21);
 // Handling value of user's input & changing the type of to 'Number'
      const guess = Number(document.querySelector('.guess').value);
      console.log(guess);
+
+      //Setting All Scenerarios
+      //Input not recived from user 
+      if (!guess){
+          document.querySelector('.message').textContent='⛔️ Must Enter A Number!';
+           //Correct Input 
+      }else if (guess === secretNumber){
+          document.querySelector('.message').textContent='🎉 Correct Number!';
+          document.querySelector('body').style.backgroundColor = '#40cd16';
+          document.querySelector('body').style.width = '30rem';
+      }
  });
 
