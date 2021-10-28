0bca7ab4cdbba1f901cca5a6fb86115967a8e3e0
Ana Ea <aabad0464@gmail.com>
2021-10-28 13:48:13 -0600

    handleling the value of users input

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ let highScore = 0;
 
 //Handle Secret Number using a random number between 0-20
 let secretNumber =Math.trunc(Math.random()* 21);
+
+//Handling Click
+ document.querySelector('.check').addEventListener('click', function(){
+// Handling value of user's input & changing the type of to 'Number'
+     const guess = Number(document.querySelector('.guess').value);
+     console.log(guess);
+ });
+
