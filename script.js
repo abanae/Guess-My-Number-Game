29e9b9f12003f16af93b080f173081e96836d608
Ana Ea <aabad0464@gmail.com>
2021-10-28 13:40:28 -0600

    setting variable for random number function

--- script.js
+++ script.js
@@ -4,3 +4,6 @@
 //Setting Score Initail value(STATE DATA)
 let score = 20;
 let highScore = 0;
+
+//Handle Secret Number using a random number between 0-20
+let secretNumber =Math.trunc(Math.random()* 21);
